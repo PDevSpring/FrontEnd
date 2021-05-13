676aec214aaaf5127e0b4c5b4b26ed55ca8fa775
ELGhoulGhazi <53657358+ELGhoulGhazi@users.noreply.github.com>
2021-05-13 01:24:58 +0100

    Revert "change bank"
    This reverts commit a7d328cd3f2b3861ed043d2b956b4071dff24c1a.

---
+++ src/app/components/banksettings/banksettings.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/Services/login.service';
+
+@Component({
+  selector: 'app-banksettings',
+  templateUrl: './banksettings.component.html',
+  styleUrls: ['./banksettings.component.css']
+})
+export class BanksettingsComponent implements OnInit {
+
+  ScriptElement: HTMLScriptElement;
+  ScriptElement1: HTMLScriptElement;
+  ScriptElement2: HTMLScriptElement;
+  ScriptElement3: HTMLScriptElement;
+  constructor(private service:LoginService,private router : Router){
+    this.ScriptElement = document.createElement('script'); 
+    this.ScriptElement.src = "../../../assets/js/main.js" ;
+    document.body.appendChild(this.ScriptElement);
+
+    this.ScriptElement1 = document.createElement('script'); 
+    this.ScriptElement1.src = "../../../assets/vendor/bootstrap/js/bootstrap.bundle.min.js" ;
+    document.body.appendChild(this.ScriptElement1);
+
+    this.ScriptElement2 = document.createElement('script'); 
+    this.ScriptElement2.src = "../../../assets/vendor/php-email-form/validate.js" ;
+    document.body.appendChild(this.ScriptElement2);
+
+    this.ScriptElement3 = document.createElement('script'); 
+    this.ScriptElement3.src = "../../../assets/vendor/swiper/swiper-bundle.min.js" ;
+    document.body.appendChild(this.ScriptElement3);
+  }
+
+
+  ngOnInit(): void {
+  }
+
+  appLogout(){
+    this.service.logoutUser();
+    this.router.navigate(['/'])
+  }
+
+
+}
